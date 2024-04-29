72e2955ee26b95874e2a6c854c2f5797250e4d6f
Liqi <liqi@mofengtech.com>
2024-04-29 18:28:34 +0800

    初始化文件

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
 // getting places from APIs
 function loadPlaces(position) {
-    const params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: '0CVJL4IA0COBMKFBKY5RYGTBGIUOSII4DCFUXNP2411XH4CP',
-        clientSecret: 'IYSHF2YZNJ1SNV12EUV2ZJ43UYMWBZBUULYZMABI5VE0DXD1',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API (limit param: number of maximum places to fetch)
-    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=30 
-        &v=${params.version}`;
-    return fetch(endpoint)
+    const endpoint = `https://api.foursquare.com/v3/places/nearby?ll=${position.latitude},${position.longitude}&limit=30`;
+    return fetch({
+        url: endpoint,
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "fsq3Xd8yruhR+Efq1YHJP4dR3s78qmDxvKkJQDUzMxfqWj8="
+        }
+    })
         .then((res) => {
             return res.json()
                 .then((resp) => {
+                    console.log(res)
                     return resp.response.venues;
                 })
         })
@@ -33,12 +23,8 @@ function loadPlaces(position) {
 
 window.onload = () => {
     const scene = document.querySelector('a-scene');
-
-    // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // than use it to load from remote APIs some places nearby
-            loadPlaces(position.coords)
+         loadPlaces(position.coords)
                 .then((places) => {
                     places.forEach((place) => {
                         const latitude = place.location.lat;
